b7fa26d71d2f889359ab1c2f5cc50759e9653a47
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-04-17 10:49:30 -0500

    CHANGES

--- src/vista/AgregarProductoCasilla.java
+++ src/vista/AgregarProductoCasilla.java
@@ -5,7 +5,6 @@ import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -15,12 +14,11 @@ import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 import javax.swing.border.EmptyBorder;
-
 import vo.Casilla;
 import vo.Producto;
 
-public class AgregarProductoCasilla extends JFrame {
-
+public class AgregarProductoCasilla extends JFrame
+{
 	private Casilla[][] maquina;
 	private JPanel contentPane;
 	private MenuOperario menuOperario;
@@ -48,15 +46,13 @@ public class AgregarProductoCasilla extends JFrame {
 	private JTextField fila;
 	private JTextField columna;
 
-	public AgregarProductoCasilla(MenuOperario menuOperario) {
+	public AgregarProductoCasilla(MenuOperario menuOperario)
+	{
 		this.menuOperario = menuOperario;
-
 		pnlMaquina = new JPanel();
 		maquina = menuOperario.getInicializarMaquina1().getMaquina();
-
 		pnlMaquina.setBounds(500, 80, 500, 400);
 		pnlMaquina.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		this.setTitle("Gestor Máquina");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(10, 10, 1200, 720);
@@ -64,11 +60,13 @@ public class AgregarProductoCasilla extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
 		Producto[] productos = verProductos();
 		String[][] data = new String[productos.length][3];
-		for (int j = 0; j < data.length; j++) {
-			if (productos[j] != null) {
+		
+		for (int j = 0; j < data.length; j++)
+		{
+			if (productos[j] != null)
+			{
 				data[j][0] = productos[j].getIdProducto() + "";
 				data[j][1] = productos[j].getNombre() + "";
 				data[j][2] = productos[j].getPrecio() + "";
@@ -76,131 +74,109 @@ public class AgregarProductoCasilla extends JFrame {
 		}
 
 		String[] columnNames = { "nombre", "apellido", "telefono" };
-
 		table = new JTable(data, columnNames);
 		table.setVisible(true);
 		table.setBounds(40, 315, 393, 150); // Aqui voy
 		contentPane.add(table);
-
 		lblPrimerMensaje = new JLabel(
 				"Para agregar un producto en la máquina" + " haga clic en la lista, luego haga clic en el espacio"
 						+ " de la máquina y haga clic en el botón Agregar Producto.");
 		lblPrimerMensaje.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblPrimerMensaje.setBounds(20, 0, 1060, 80);
 		contentPane.add(lblPrimerMensaje);
-
 		lblSegundoMensaje = new JLabel("Para quitar un producto del espacio de la"
 				+ " máquina haga clic en la casilla de la máquina y posteriormente"
 				+ " haga clic en la opción QuitarProducto.");
 		lblSegundoMensaje.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblSegundoMensaje.setBounds(20, 20, 1050, 80);
 		contentPane.add(lblSegundoMensaje);
-
 		lblTercerMensaje = new JLabel("Lista de productos");
 		lblTercerMensaje.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		lblTercerMensaje.setBounds(132, 230, 1050, 80);
 		contentPane.add(lblTercerMensaje);
-
 		txtpnIdProducto = new JTextPane();
 		txtpnIdProducto.setText("Identificaci\u00F3n Producto");
 		txtpnIdProducto.setBounds(40, 295, 142, 20);
 		contentPane.add(txtpnIdProducto);
-
 		txtpnNombreproducto = new JTextPane();
 		txtpnNombreproducto.setText("  Nombre Producto");
 		txtpnNombreproducto.setBounds(180, 295, 122, 20);
 		contentPane.add(txtpnNombreproducto);
-
 		txtpnPrecio = new JTextPane();
 		txtpnPrecio.setText("               Precio");
 		txtpnPrecio.setBounds(300, 295, 131, 20);
 		contentPane.add(txtpnPrecio);
-
 		agregarProducto = new JButton("Agregar");
 		agregarProducto.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		agregarProducto.setBounds(450, 640, 190, 23);
 		contentPane.add(agregarProducto);
-
 		quitarProducto = new JButton("Quitar");
 		quitarProducto.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		quitarProducto.setBounds(650, 640, 132, 23);
 		contentPane.add(quitarProducto);
-
 		lblIdProducto = new JLabel("IdProducto");
 		lblIdProducto.setBounds(40, 480, 89, 14);
 		contentPane.add(lblIdProducto);
-
 		jIdProducto = new JTextField();
 		jIdProducto.setBounds(115, 475, 97, 28);
 		contentPane.add(jIdProducto);
-
 		lblNombreProducto = new JLabel("Nombre Producto");
 		lblNombreProducto.setBounds(40, 515, 107, 14);
 		contentPane.add(lblNombreProducto);
-
 		jNombre = new JTextField();
 		jNombre.setBounds(143, 510, 97, 28);
 		contentPane.add(jNombre);
-
 		lblprecio = new JLabel("Precio");
 		lblprecio.setBounds(40, 554, 107, 14);
 		contentPane.add(lblprecio);
-
 		jPrecio = new JTextField();
 		jPrecio.setBounds(86, 550, 97, 28);
 		contentPane.add(jPrecio);
-
 		regresar = new JButton("Regresar");
 		regresar.setBounds(340, 639, 97, 23);
 		contentPane.add(regresar);
-
 		fila = new JTextField();
 		fila.setBounds(65, 590, 97, 28);
 		contentPane.add(fila);
-
 		columna = new JTextField();
 		columna.setBounds(97, 625, 97, 28);
 		contentPane.add(columna);
-		
 		lblfila = new JLabel("fila");
 		lblfila.setBounds(40, 600, 107, 14);
 		contentPane.add(lblfila);
-		
 		lblcolumna = new JLabel("columna");
 		lblcolumna.setBounds(40, 633, 107, 14);
 		contentPane.add(lblcolumna);
-		/*
-		 * lblNombreProducto = new JLabel("Nombre Producto");
-		 * lblNombreProducto.setBounds(40, 335, 393, 150);
-		 * contentPane.add(lblNombreProducto);
-		 * 
-		 * lblprecio = new JLabel("IdProducto"); lblprecio.setBounds(40, 365, 393, 150);
-		 * contentPane.add(lblprecio);
-		 */
-
 		eventos();
 		mostrarMaquina();
 	}
 
-	public void eventos() {
-		agregarProducto.addMouseListener(new MouseAdapter() {
+	public void eventos()
+	{
+		agregarProducto.addMouseListener(new MouseAdapter()
+		{
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
+			public void mouseClicked(MouseEvent arg0)
+			{
 
 				modificarCasilla(Integer.parseInt(fila.getText()), Integer.parseInt(columna.getText()));
 			}
 		});
 
-		quitarProducto.addMouseListener(new MouseAdapter() {
+		quitarProducto.addMouseListener(new MouseAdapter()
+		{
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent e)
+			{
 				quitarProductoCasilla(Integer.parseInt(fila.getText()), Integer.parseInt(columna.getText()));
 			}
 		});
-		// evento Seleccionar tabla
-		table.addMouseListener(new MouseAdapter() {
+
+		table.addMouseListener(new MouseAdapter()
+		{
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent e)
+			{
 				int seleccion = table.rowAtPoint(e.getPoint());
 				jIdProducto.setText(String.valueOf(table.getValueAt(seleccion, 0)));
 				jNombre.setText(String.valueOf(table.getValueAt(seleccion, 1)));
@@ -208,27 +184,32 @@ public class AgregarProductoCasilla extends JFrame {
 			}
 		});
 
-		regresar.addMouseListener(new MouseAdapter() {
+		regresar.addMouseListener(new MouseAdapter()
+		{
 			@Override
-			public void mouseClicked(MouseEvent e) {
+			public void mouseClicked(MouseEvent e)
+			{
 				menuOperario.setVisible(true);
 				setVisible(false);
 			}
 		});
 	}
 
-	public void eventoModificarCasilla() {
-		boton.addMouseListener(new MouseAdapter() {
+	public void eventoModificarCasilla()
+	{
+		boton.addMouseListener(new MouseAdapter()
+		{
 			@Override
-			public void mouseClicked(MouseEvent arg0) {
-
+			public void mouseClicked(MouseEvent arg0)
+			{
 			}
 		});
 	}
 
-	public void modificarCasilla(int fila, int columna) {
-
-		if ((fila >= 1 && fila <= maquina.length) && (columna >= 1 && columna <= maquina[0].length)) {
+	public void modificarCasilla(int fila, int columna)
+	{
+		if ((fila >= 1 && fila <= maquina.length) && (columna >= 1 && columna <= maquina[0].length))
+		{
 			Casilla casilla = maquina[fila - 1][columna - 1];
 			JButton boton = maquina[fila - 1][columna - 1].getBoton();
 
