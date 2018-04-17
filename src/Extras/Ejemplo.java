aabc60633623fb43469159f0cbfdd10b1f42c665
BrianS01 <brian.sterling@correo.usa.edu.co>
2018-04-17 10:42:00 -0500

    CHANGES

--- src/Extras/Ejemplo.java
+++ src/Extras/Ejemplo.java
@@ -8,32 +8,31 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 
-
 public class Ejemplo extends JFrame
 {
-
 	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
+	
+	public static void main(String[] args)
+	{
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
 					Ejemplo frame = new Ejemplo();
 					frame.setVisible(true);
-				} catch (Exception e) {
+				}
+				catch (Exception e)
+				{
 					e.printStackTrace();
 				}
 			}
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public Ejemplo() {
+	public Ejemplo()
+	{
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
